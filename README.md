8dbd557cc53cd682867f11ede3b68d421b8dea1e
tosha <anton.borissov@gmail.com>
2016-07-17 21:06:48 -0400

    MPS Reloaded

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
 # mps
+
+--------------------------------------------------------------------------
+
+## Tue Jun 21, 2016
+
+    - the first code iteration was messy and not organized
+    - began second iteration
+    - goals: make modular and relatively well-documented 
+             (at the very least readable)
+    - Interesting Discoveries: multi_array<int,0> is supported and is
+    accessed using
+
+            a(vector<int>())
+    - Questions: What is a const_iterator?
+
+    - next: testing contract.cpp
+
+--------------------------------------------------------------------------
+
+## Wed Jun 22, 2016
+
+    - contract.cpp seems to be working - no bugs found yet
+    - trying to organize code (move some files into header)
+
+--------------------------------------------------------------------------
+
+## Thu Jun 23, 2016
+
+    - contract_check1 and contract_check2 work! (or they were working...)
+    - completely mangled the templates and now the errors are unreadable:
+    full of instantiation problems.
+
+--------------------------------------------------------------------------
+
+## Wed Jul 13, 2016
+
+    I'm back! Feynman diagram project now done. Time to focus efforts on
+    coding for a bit.
+
+    - Finished: explanation of dmrg using mps
+        (~/Documents/masters/mps/mps-notes.pdf)
+    - Found the reason why the instantiation problems were arising:
+        -> added a new "feature": barA, barB, selection could all be
+        vector-like containers, however, implemented this with templates
+        WITHIN the cpp file (usually templated-functions need to be
+                implemented in the header files)
+        -> solution: 
+            (a) enforce a vector structure for all such 
+                index-type containers
+            (b) enforce templated-functions ALWAYS go in header file
+
+--------------------------------------------------------------------------
+
+## Thu Jul 14, 2016
+
+    - Question: Just renaming tests.cpp -> test.h fixed a
+    multiple-definition problem; why?
+
+    - All tests work. 
+
+    - Question: Why in did I make barA and barB array's!?!
+    - Question: Template instantiation requires a lot of overhead (compile
+                    time is "supposedly" increased)
+                I have not bumped into this, but is this true?
+                Is there any way to mitigate this and save on compile time
+
+    - MPS notes say that there are four things to implement:
+        1. Full state -> MPS (implement epsilon-cutoff)
+        2. Structure for contraction of < psi | psi > and  
+            < psi | H | psi >
+        3. Reformat tensor to matrix to tensor
+        4. Generalized eigenvalue problem
+        
+    - Decreasing difficulty: 4,2,1,3
+
+    - Began implementing 3; ran into a few problems with Eigen
+    	-> solution: take the time to read Eigen's documentation fully
+		     it will surely be beneficial later on!!!
+
+--------------------------------------------------------------------------
+
+## Fri Jul 16 2016
+
+    - Finished Reading Eigen documentation
+
+--------------------------------------------------------------------------
+
+## Sun Jul 17 2016
+
+    - Learn to implement operator<< for vector<int>
+    - All tests for   tensor_to_matrix,  matrix_to_tensor   
+      passed successfully.
+    - July 14 - #3 done.
+
+    - Next goal: J14 #4 (generalized eigenvalue problem)
+      which is another self-contained problem!
+         ----> SOLVED!
+
+         Eigen::GeneralizedSelfAdjointEigenSolver(H,N)
+         where $H$ is the Hamiltonian and $N$ is the auxillary matrix
+    - July 14 - #4 done.
+
+    - Next goal: J14 #2 -- restructure < psi | psi >
+
+
--- tensor.h
+++ tensor.h
@@ -1,262 +1,121 @@
+
+#ifndef MPS_TENSOR_H
+#define MPS_TENSOR_H
+
+
 #include <iostream>
-#include <vector>
+#include <algorithm>
 #include <boost/multi_array.hpp>
 #include <array>
-#include <functional>
-#include <utility>
-#include <armadillo>
+#include <iterator>
+#include <vector>
+#include <complex>
+#include <Eigen/Dense>
 
+#include "increment_indices.h"
+#include "reindexing.h"
 
 template<typename T, std::size_t N>
 using tensor = boost::multi_array<T,N>;
 
-
-
-/*****************************************
- * Output contents of N-component tensor *
- *****************************************/
-template<typename T, std::size_t N> 
-void output(boost::multi_array<T,N> a)
-{
-    auto f = [] (int x) { std::cout << x << " "; };
-    std::for_each(a.origin(), a.origin()+a.num_elements(), f);
+template<std::size_t N, typename T>
+void output_tensor(tensor<T,N> t) {
+    copy(t.origin(),t.origin()+t.num_elements(), std::ostream_iterator<T>(std::cout, " "));
     std::cout << std::endl;
 }
 
-std::vector<int> int_to_index(int k, std::vector<int> shape)
+template<typename T, std::size_t N, std::size_t M, std::size_t dA, std::size_t dB>
+    tensor<T,N+M-dA-dB> 
+contract(tensor<T,N> A, tensor<T,M> B, std::array<int,dA> barA, std::array<int,dB> barB)
 {
-    // "i" enumerates the index abc
-    // the following does: "i" -> abc
-    int prod = std::accumulate(shape.begin(),shape.end(), 1, std::multiplies<int>());
-    std::vector<int> vec_index(shape.size());
-    for(int j = 0; j < shape.size(); ++j){
-        vec_index[j] = (k% shape[j]);
-        k -= vec_index[j]; 
-        k /= shape[j];
-    }
-    return vec_index;
+
+    assert(barA.size() == barB.size());
+    for (int k = 0; k < barA.size(); ++k)
+        assert(A.shape()[barA[k]] == B.shape()[barB[k]]);
+
+    std::vector<int> vecA(A.shape(), A.shape()+N);       // vecA, vecB == shape of A, B
+    std::vector<int> vecB(B.shape(), B.shape()+M);       //
+    std::vector<int> vecC(N+M-dA-dB);                    // 
+    std::vector<int> indA(vecA.size());                  // indA, indB == indices of A, B
+    std::vector<int> indB(vecB.size());                  // these variables are looped over below
+    std::vector<int> indC(N+M-dA-dB);                    // indC = [ Union(indA,indB) - Overlap ]
+
+    combine(vecC,vecA,vecB,barA,barB);
+    tensor<T,N+M-dA-dB> C(vecC);
+
+    do {
+        do {
+
+            T sum = 0;
+
+            do {
+
+                sum += A(indA)*B(indB);
+                
+                increment_index_with_selection(indA, vecA, barA);
+                increment_index_with_selection(indB, vecB, barB);
+
+            }while(!check_all_zeros_with_selection(indA,barA));
+
+
+            // set value of C
+            combine(indC, indA, indB, barA, barB);
+            C(indC) = sum;
+
+            // increment the outside
+            increment_index_with_barrier(indB,vecB,barB);
+
+        } while(!check_all_zeros_with_barrier(indB,barB));
+
+        increment_index_with_barrier(indA,vecA,barA);
+    } while(!check_all_zeros_with_barrier(indA,barA));
+
+    return C;
 }
 
-template<typename T, std::size_t N, std::size_t M> 
-boost::multi_array<T, N+M> 
-multiply_tensors(boost::multi_array<T,N> t1, boost::multi_array<T,M> t2)
+template<size_t R, size_t C, typename T>
+    Eigen::MatrixXcd
+tensor_to_matrix(tensor<T,R+C> A, std::array<int,R> row_indices, std::array<int,C> col_indices)
 {
-    // declares the components of tensor
-    std::vector<int> index; index.reserve(N+M);
-    for (auto p = t1.shape(); p != t1.shape()+N; ++p)
-        index.push_back(*p);
-    for (auto p = t2.shape(); p != t2.shape()+M; ++p)
-        index.push_back(*p);
+    //dimensions of Matrix
+    int Row = 1;
+    int Col = 1;
+    for (auto i : row_indices) Row *= A.shape()[i];
+    for (auto i : col_indices) Col *= A.shape()[i];
 
-    boost::multi_array<T, N+M> new_tensor(index);
+    Eigen::MatrixXcd M(Row,Col);
+    std::vector<int> ind(R+C); // initially set to zero
 
-    // eg. Z_abc = X_a*Y_bc
-    for (int i = 0; i < new_tensor.num_elements(); ++i){
-        // "i" enumerates the index abc
-        // the following does: "i" -> abc
-        int k = i;
-        std::vector<int> i_index(N+M);
-        for(int j = 0; j < N+M; ++j){
-            i_index[j] = (k% (new_tensor.shape())[j]);
-            k -= i_index[j];
-            k /= (new_tensor.shape())[j];
-        }
-        new_tensor(i_index) = 
-            t1( std::vector<int>(i_index.begin(),i_index.begin()+N) )
-          * t2( std::vector<int>(i_index.begin()+N,i_index.end()) );
-    }
-    return new_tensor;
-}
+    do {
+        int a = multi_index_to_number(reindexing_subset(ind, row_indices), reindexing_subset(A.shape(), row_indices));
+        int b = multi_index_to_number(reindexing_subset(ind, col_indices), reindexing_subset(A.shape(), col_indices));
 
+        M(a,b) = A(ind);
 
-/* ****************************************************************** * 
- *  eg. F_abca                                                        *
- *  This method contracts one pair of indices within a single tensor. *
- * ****************************************************************** */
-template<typename T, std::size_t N>
-boost::multi_array<T,N-2> 
-contract(boost::multi_array<T,N> t1, int x, int y){
-    /*  eg. F_abca 
-     *  This method contracts one pair of indices within a single tensor.
-     */
-
-    if ((t1.shape())[x] != (t1.shape())[y]) throw;
-
-    std::vector<int> new_index;
-    new_index.reserve(N-2);
-    new_index.insert(new_index.end(), t1.shape(), t1.shape()+x);
-    new_index.insert(new_index.end(), t1.shape()+x+1,   t1.shape()+x+y);
-    new_index.insert(new_index.end(), t1.shape()+x+y+1, t1.shape()+t1.num_dimensions());
-
-    boost::multi_array<T,N-2> ans(new_index);
-    //
-    // now we use new_index and old_index to generate the new tensor
-    std::vector<int> old_index(N);
-    for(int i = 0; i < ans.num_elements(); ++i){
-        new_index = int_to_index(i, std::vector<int>(ans.shape(),ans.shape()+ans.num_dimensions()));
-        ans(new_index) = 0;
-        for (int j = 0; j < (t1.shape())[x]; ++j){
-            old_index[x] = j; old_index[y] = j;
-            copy(new_index.begin(),     new_index.begin()+x,   old_index.begin());
-            copy(new_index.begin()+x,   new_index.begin()+y-1, old_index.begin()+x+1);
-            copy(new_index.begin()+y-1, new_index.end(),       old_index.begin()+y+1);
-
-            ans(new_index) += t1(old_index);
-        }
-    }
-    return ans;
-}
-
-/*****************************
- * Tensor to matrix function *
- *****************************/
-template<typename T, std::size_t N>
-arma::Mat<T> tensor_to_matrix(boost::multi_array<T,N> t, 
-        std::vector<int> first_index, 
-        std::vector<int> second_index)
-{
-    int a = std::accumulate(first_index.begin(), first_index.end(), 1,
-            [&t](int x, int y){ return x * (t.shape())[y]; });
-    int b = std::accumulate(second_index.begin(), second_index.end(), 1,
-            [&t](int x, int y){ return x * (t.shape())[y]; });
-
-    arma::Mat<T> M(a,b);
-
-    std::vector<int> a_shape(first_index.size());
-    std::vector<int> b_shape(second_index.size());
-    for(int i = 0; i < first_index.size(); ++i)    // a_shape definition = pull back of t_shape by first_index
-        a_shape[i] = (t.shape())[first_index[i]];
-    for(int j = 0; j < second_index.size(); ++j)
-        b_shape[j] = (t.shape())[first_index[j]];
-
-    std::vector<int> a_index(first_index.size());
-    std::vector<int> b_index(second_index.size());
-    std::vector<int> t_index(t.num_dimensions());
-    for (int i = 0; i < a; ++i){
-        a_index = int_to_index(i,a_shape);         // a_index < a_shape
-        for (int k = 0; k < first_index.size(); ++k)
-            t_index[first_index[k]] = a_index[k];  // t_index == pushforward of (a_index) by (first_index)
-        for(int j = 0; j < b; ++j){
-            b_index = int_to_index(j,b_shape);     // b_index < b_shape
-            for (int k = 0; k < second_index.size(); ++k)
-                t_index[second_index[k]] = b_index[k];//t_index = pushforward 
-
-            M(i,j) = t(t_index);
-        }
-    }
+        increment_index(ind, A.shape());
+    } while(!check_all_zeros(ind));
     return M;
 }
 
 
- /**********************************************************************
- * Matrix to Tensor
- *      M_{ (a1a2..) (b1b2..) } -> T_{a1 b1 b2 a2 a3 b3 a4 ...}
- *
- *      the first component of M may not necessarily correspond to the
- *      first few components of the tensor (first_index tells us where
- *      to put the a_i's; first_shape tells us how big the dimension of
- *      each first_index has)
- ***********************************************************************/
-template<typename T, std::size_t N>
-boost::multi_array<T,N> matrix_to_tensor(arma::Mat<T> A, 
-        std::vector<int> first_shape,
-        std::vector<int> first_index, 
-        std::vector<int> second_shape, 
-        std::vector<int> second_index)
+template<size_t R, size_t C>
+    tensor<std::complex<double>,R+C>
+matrix_to_tensor(Eigen::MatrixXcd M, std::array<int,R> row_indices, std::array<int,C> col_indices, std::vector<int> shape)
 {
-    // ***_shape == the size of each component
-    // ***_index == where in the tensor it is placed
-    // Therefore the sizes of these vectors must be equal!
-    assert(first_shape.size() == first_index.size());
-    assert(second_shape.size() == second_index.size()); 
-    assert(N == first_index.size()+second_index.size());
 
-    // Make sure the dimensions of the first and second component of A
-    // will indeed bijectively correspond to the proposed tensor indices  
-    assert(arma::size(A)[0] == 
-        std::accumulate(first_shape.begin(),first_shape.end(), 1, std::multiplies<int>()));
-    assert(arma::size(A)[1] == 
-        std::accumulate(second_shape.begin(),second_shape.end(), 1, std::multiplies<int>()));
+    tensor<std::complex<double>,R+C> A(shape);
+    std::vector<int> ind(R+C); // initially set to zero
 
-    // Initialize the shape of the tensor
-    std::vector<int> total_shape(first_shape.size()+second_shape.size());
-    for(int i = 0; i < first_index.size(); ++i)
-        total_shape[first_index[i]] = first_shape[i];
-    for(int j = 0; j < second_index.size(); ++j)
-        total_shape[first_index[j]] = first_shape[j];
+    do {
+        int a = multi_index_to_number(reindexing_subset(ind, row_indices), reindexing_subset(A.shape(), row_indices));
+        int b = multi_index_to_number(reindexing_subset(ind, col_indices), reindexing_subset(A.shape(), col_indices));
 
-    boost::multi_array<T,N> final_tensor(total_shape);
+        A(ind) = M(a,b);
 
-    std::vector<int> tmp1_shape(first_shape.size());
-    std::vector<int> tmp2_shape(second_shape.size());
-    std::vector<int> tmp_total_shape(first_shape.size()+second_shape.size());
-
-    for(int i = 0; i < arma::size(A)[0]; ++i){
-        tmp1_shape = int_to_index(i,first_shape);
-        for(int k = 0; k < first_index.size(); ++k)
-            tmp_total_shape[first_index[k]] = tmp1_shape[k];
-        for (int j = 0; j < arma::size(A)[1]; ++j){
-            tmp2_shape = int_to_index(j,second_shape);
-            for(int k = 0; k < second_index.size(); ++k)
-                tmp_total_shape[second_index[k]] = tmp2_shape[k];
-            
-            // initialize the final_tensor
-            final_tensor(tmp_total_shape) = A(i,j);
-        }
-    }
-    return final_tensor;
+        increment_index(ind, A.shape());
+    } while(!check_all_zeros(ind));
+    return A;
 }
 
 
-/*********************
-* Left Normalize MPS *
-*********************/
-template<typename T>
-std::vector<tensor<T,3> > left_normalize_MPS (std::vector<tensor<T,3> > MPS)
-{
-    // Tested? No
-
-    // Follows Section 4.4 
-    // (Bringing a matrix product state into canonical form)
-
-    // NOTE: Resizing is not implemented yet.
-    // NOTE: MPS[i]     = M^{sg}_{a,b}  ==    M_[a,sg,b]
-    //  notation:          paper             tensor implementation
-    
-    arma::Mat<T> U;
-    arma::Col<T> s;
-    arma::Mat<T> V;
-
-    for (int i = 0; i < MPS.size()-1; ++i)
-    {
-        arma::svd(U,s,V,
-                tensor_to_matrix(MPS[i],std::vector<int>{0,1},std::vector<int>{2}));
-        MPS[i]   = matrix_to_tensor(U,
-                      std::vector<int> { MPS[i].shape()[0], MPS[i].shape()[1] },  //first_shape
-                      std::vector<int> {         0        ,         1         },  //first_index
-                      std::vector<int> {               s.size()               },  //second_shape
-                      std::vector<int> {                  2                   }); //second_index
-        MPS[i+1] = contract( multiply_tensors( matrix_to_tensor(arma::diagmat(s)*V.t(),
-                                                    std::vector<int> { s.size() },  //first_shape
-                                                    std::vector<int> {     0    },  //first_index
-                                                    std::vector<int> { s.size() },  //second_shape
-                                                    std::vector<int> {     1    }), //second_index
-                                               MPS[i+1]),
-                             1,2); // indices to contract 
-        // Explanation:
-        // SV_{s,a} * M^{sg}_{a,b} => Tensor_[s,a,a,sg,b] == Tensor_[0,1,2,3,4] 
-        // contract -> Tensor_[s,sg,b]
-    }
-    return MPS;
-}
-
-/***********************
-* Vidal Representation *
-***********************/
-template<typename T>
-std::vector<tensor<T,3> > MPS_to_Vidal_Form (std::vector<tensor<T,3> > MPS)
-{
-    // 2L+1 tensors in vidal form
-    // odd-indexed (using 0,1,.. convention) are the diagonal elements with
-    //      tensor structure T_[a,1,b], effectively a 2-tensor
-}
+#endif // MPS_TENSOR_H
